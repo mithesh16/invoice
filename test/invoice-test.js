890becbd36405faa9c7b1e9f655b06aa2b921e18
Mithesh Srinivasan <93369383+mithesh16@users.noreply.github.com>
2022-12-24 11:39:23 +0530

    Update invoice-test.js

--- test/invoice-test.js
+++ test/invoice-test.js
@@ -1,28 +1,41 @@
-const { expect } = require("chai");
+const {expect}=require('chai');
 
-describe("invoice", async function () {
-    const testdata=['buyer','seller',10,'12.12.22']
-    const Contract = await ethers.getContractFactory("invoice");
-    const contract = await Contract.deploy();
+describe("Invoice testing",function(){
+  let contract;
+  let accounts;
+  const sellerpan="123"
+  const buyerpan="456"
+  const amt=10
+  const date="24.12.22"
 
-   console.log(contract.address);
-   
-   it("Adding new invoice to the ledger and checking it",async function(){
-      const txresponse=await contract.addTransaction(testdata[0],testdata[1],testdata[2],testdata[3]);
-      txresponse.wait();
-     const transaction=await contract.showTransaction(1);
-      expect(transaction[1].to.equal(testdata[0]));
-      expect(transaction[2].to.equal(testdata[1]));
-      expect(transaction[3].to.equal(testdata[2]));
-      expect(transaction[4].to.equal(testdata[3]));
-    });
-   
-    it("Checks for buyer history",async function(){
-      const txdata=await contract.getBuyerhistory('buyer');
-      expect(txdata[0][1].to.equal(testdata[0]));
-      expect(txdata[0][2].to.equal(testdata[1]));
-      expect(txdata[0][3].to.equal(testdata[2]));
-      expect(txdata[0][4].to.equal(testdata[3]));
-    })
+  const sellerpan2="1234"
+  const amt2=12
+  const date2="25.12.22"
 
-  });
+
+
+  const testdata=[[buyerpan,sellerpan,amt,date],[buyerpan,sellerpan2,amt2,date2]]
+
+
+  this.beforeAll(async function(){
+    accounts=await ethers.getSigners()
+    const owner=accounts[0]
+    const account=accounts[1]
+      const Contract=await hre.ethers.getContractFactory("invoice")
+      contract=await Contract.deploy();
+      await contract.deployed();
+  })
+  it("Should add a transaction",async function(){
+        
+        const tx=await contract.addTransaction(buyerpan,sellerpan,amt,date);
+        const result=await contract.showTransaction(0);
+        let _buyerpan=result[1]
+        let _sellerpan=result[2]
+        let _amt=result[3].toString()
+        let _date=result[4]
+
+        expect(buyerpan).to.equal(_buyerpan);
+        expect(sellerpan).to.equal(_sellerpan)
+        expect(amt.toString()).to.equal(_amt)
+        expect(date).to.equal(_date)   
+  })
