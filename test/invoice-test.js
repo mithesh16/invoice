730f6b80ee404c5beeb99b5e761eddfb524aeeb3
Mithesh Srinivasan <93369383+mithesh16@users.noreply.github.com>
2022-12-24 11:43:48 +0530

    Update invoice-test.js

--- test/invoice-test.js
+++ test/invoice-test.js
@@ -39,3 +39,31 @@ describe("Invoice testing",function(){
         expect(amt.toString()).to.equal(_amt)
         expect(date).to.equal(_date)   
   })
+  it("Should return the buyer history",async function(){
+    const tx=await contract.addTransaction(buyerpan,sellerpan,amt,date);
+    const tx2=await contract.addTransaction(buyerpan,sellerpan2,amt2,date2);
+    const result=await contract.getBuyerHistory(buyerpan);
+   
+      let _buyerpan=result[0][1]
+        let _sellerpan=result[0][2]
+        let _amt=result[0][3].toString()
+        let _date=result[0][4]
+        
+        expect(buyerpan).to.equal(_buyerpan);
+        expect(sellerpan).to.equal(_sellerpan)
+        expect(amt.toString()).to.equal(_amt)
+        expect(date).to.equal(_date)  
+
+         _buyerpan=result[2][1]
+         _sellerpan=result[2][2]
+         _amt=result[2][3].toString()
+        _date=result[2][4]
+        
+        expect(buyerpan).to.equal(_buyerpan);
+        expect(sellerpan2).to.equal(_sellerpan)
+        expect(amt2.toString()).to.equal(_amt)
+        expect(date2).to.equal(_date)  
+    
+  })
+})
+
